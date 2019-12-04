2f61316916c40e9819f68ea5d2c8429c93686f16
Jorge Winder <jorge.winder@gmail.com>
2019-12-03 19:39:02 -0500

    Se agregaron los ultimos cambios

--- server-inicial.js
+++ server-inicial.js
@@ -1,7 +1,6 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const response = require('./network/response')
 const router = express.Router()
 
 var app = express()
@@ -11,34 +10,23 @@ var app = express()
 // })
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParsergit .urlencoded({extended: false}))
 app.use(router)
 
-router.get('/mensaje', function(req, res){
+router.get('/', function(req, res){
     console.log(req.headers)
     res.header({
         "custom-header": "Nuestro valor predeterminado"
     })
-    //res.send('Llamado a get')
-    response.success(req, res)
-
+    res.send('Llamado a get')
 })
 
 router.post('/mensaje', function(req, res){
     console.log(req.query)
     console.log(req.body)
     //res.send('LLamado post ' + req.body.Nombre + ' con DNI: ' + req.query.dni)
-    //res.status(201).send({error: "", body: "Creado correctamente"})   
-
-    if(req.query.error == 'ok'){
-        response.error(req, res, 'Error simulado')
-    }else{
-        response.success(req, res, 'Creado correctamente', 201)
-    }
-    
+    res.status(201).send({error: "", body: "Creado correctamente"})    
 })
-
-app.use('/app',express.static('public'))
 
 app.listen(3000)
 console.log('Servidor NodeJS en escucha en http://localhost:3000')
