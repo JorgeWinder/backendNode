5de17a3b8e53781a511997dac7475eebb04d7f43
Jorge Winder <jorge.winder@gmail.com>
2019-12-26 15:57:15 -0500

    final curso backend nodejs

--- network/routers.js
+++ network/routers.js
@@ -2,7 +2,6 @@ const express = require('express')
 const message = require('../components/message/network')
 
 const routers = function(server){
-    // Cada vez que nuestro server(APP) llame a '/message' llamará a nuestro componente de message
     server.use('/message', message)
 }
 
