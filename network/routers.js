be25a5e708c2c2e85a81455f38dc6f038e3c0dc0
Jorge Winder <jorge.winder@gmail.com>
2020-01-03 12:46:57 -0500

    Últimos cambios en el master

--- network/routers.js
+++ network/routers.js
@@ -2,6 +2,7 @@ const express = require('express')
 const message = require('../components/message/network')
 
 const routers = function(server){
+    // Cada vez que nuestro server(APP) llame a '/message' llamará a nuestro componente de message
     server.use('/message', message)
 }
 
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+
 //const router = require('./components/message/network')
 const router = require('./network/routers')
 
 var app = express()
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 //app.use(router)
