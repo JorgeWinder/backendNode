877ec1458a15c46846aea0854e3b0c4535e71256
Jorge Winder <41927307+JorgeWinder@users.noreply.github.com>
2020-05-21 01:14:10 -0500

    Update server-v1.js

--- server-v1.js
+++ server-v1.js
@@ -10,7 +10,7 @@ var app = express()
 // })
 
 app.use(bodyParser.json())
-app.use(bodyParsergit .urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(router)
 
 router.get('/', function(req, res){
